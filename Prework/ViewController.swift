a63c561bc478ade7078dfa12455d3b5c2bffc845
christeo2001 <christeo2001@hotmail.com>
2021-07-04 02:34:03 -0400

    Initial Commit

---
+++ Prework/ViewController.swift
@@ -0,0 +1,71 @@
+//
+//  ViewController.swift
+//  Prework
+//
+//  Created by Christian Teo on 7/3/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    @IBOutlet weak var billAmountTextField: UITextField!
+    @IBOutlet weak var tipAmountLabel: UILabel!
+    @IBOutlet weak var tipControl: UISegmentedControl!
+    @IBOutlet weak var totalLabel: UILabel!
+    @IBOutlet weak var tipSlider: UISlider!
+    @IBOutlet weak var sliderLabel: UILabel!
+
+
+    @IBAction func sliderValueChanged(_ sender: UISlider) {
+        let currentValue = Int(sender.value * 100)
+        sliderLabel.text = "\(currentValue)%"
+        
+        // Get bill amount from text field input
+        let bill = Double(billAmountTextField.text!) ?? 0
+        
+        let tip = Double(currentValue) * bill / 100
+        
+        let total = bill + tip
+        
+        // Update Tip Amount Label
+        tipAmountLabel.text = String(format: "$%.2f", tip)
+        // Update Total Amount
+        totalLabel.text = String(format:"$%.2f", total)
+        
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+        self.title = "Tip Calculator"
+    }
+
+    @IBAction func calculateTip(_ sender: Any) {
+
+        // Get bill amount from text field input
+        let bill = Double(billAmountTextField.text!) ?? 0
+        
+        
+        // Get total tip by multiplying tip * tipPercentage
+        let tipPercentages = [0.15, 0.18, 0.20]
+        
+        let tip = bill * tipPercentages[tipControl.selectedSegmentIndex]
+        
+        sliderLabel.text = "\(Int(tipPercentages[tipControl.selectedSegmentIndex] * 100))%"
+        
+        let total = bill + tip
+        
+        tipSlider.value = Float(tipPercentages[tipControl.selectedSegmentIndex])
+
+        // Update Tip Amount Label
+        tipAmountLabel.text = String(format: "$%.2f", tip)
+        // Update Total Amount
+        totalLabel.text = String(format:"$%.2f", total)
+        
+
+    }
+
+}
+
+
---
+++ SettingsViewController.swift
@@ -0,0 +1,35 @@
+//
+//  SettingsViewController.swift
+//  Prework
+//
+//  Created by Christian Teo on 7/3/21.
+//
+
+import UIKit
+
+class SettingsViewController: UIViewController {
+
+    @IBOutlet weak var defaultTipTextField: UITextField!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func defaultTipTextEditor(_ sender: UITextField) {
+        
+    }
+
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
